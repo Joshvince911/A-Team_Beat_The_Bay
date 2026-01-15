7ee3b2ee046a916d9383bf48695a7ce9222f7c79
ArnavYadav-csper5 <566782@my.cuhsd.org>
2026-01-14 16:31:12 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -9,6 +9,8 @@
 
 # Library imports
 from vex import *
+import motor
+import motor_group
 
 brain = Brain()
 controller_1 = Controller()
@@ -77,4 +79,57 @@ def user_control():
 comp = Competition(user_control, autonomous)
 
 # actions to do when the program starts
+
 brain.screen.clear_screen()
+
+# Setting up motors and controller
+
+# Left side motors
+motor_1a = vex.Motor(vex.Ports.PORT1)
+motor_2a = vex.Motor(vex.Ports.PORT2)
+motor_3a = vex.Motor(vex.Ports.PORT3)
+
+# Right side motors
+motor_1b = vex.Motor(vex.Ports.PORT4)
+motor_2b = vex.Motor(vex.Ports.PORT5)
+motor_3b = vex.Motor(vex.Ports.PORT6)
+
+# Initalize motor groups
+motor_group_1 = motor_group(motor_1a, motor_2a, motor_3a)
+motor_group_2 = motor_group(motor_1b, motor_2b, motor_3b)
+
+#Initialize controller
+controller_1 = Controller()
+
+# Set up controller buttons
+while True:
+    # Setting up arrow buttons for controlling the left motor group
+    
+    # If the up arrow button is pressed, the left motor group spins forward
+    if controller_1.buttonUp.pressing():
+        motor_group_1.spin(FORWARD,100)
+        
+    # If the down arrow button is pressed, the left motor group spins backwards
+    elif controller_1.buttonDown.pressing():
+        motor_group_1.spin(REVERSE,100)
+        
+    # If neither of these buttons are pressed the left motor group stops
+    else:
+        motor_group_1.stop()
+    
+    # Setting up the letter buttons (A, B, X, & Y) for the right motor group
+    
+    # If the X button is pressed, the right motor group spins forward
+    if controller_1.buttonX.pressing():
+        motor_group_2.spin(FORWARD,100)
+        
+    # If the B button is pressed, the right motor group spins backward
+    elif controller_1.buttonB.pressing():
+        motor_group_2.spin(REVERSE,100)
+        
+    # If neither of these buttons are pressed, the right motor group stops
+    else:
+        motor_group_2.stop()
+    
+    # A small wait is necessary to prevent the brain from getting overwhelmed and crashing
+    wait(5, MSEC)
